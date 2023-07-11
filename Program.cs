37b04c5d158eca271976d684131005fc57b407cb
rastogi102 <nikhilrastogi102@gmail.com>
2023-07-11 11:58:26 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoToExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string userName;
+            Again:
+            Console.WriteLine("Enter UserName");
+            userName=Console.ReadLine();
+            if(userName.Length>=6) 
+            {
+                Console.WriteLine("Welcome : \t" + userName);
+
+            }
+            else
+            {
+                Console.WriteLine("Invalid UserName");
+                goto Again;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
